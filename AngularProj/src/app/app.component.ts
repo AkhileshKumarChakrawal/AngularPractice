ffb8ac082588a7aa9d118ac6af04d95fc788bafd
akhilesh <akhilesh.chakrawal@india.semanticbits.com>
2020-02-05 19:18:36 +0530

    @input and @output and service

--- AngularProj/src/app/app.component.ts
+++ AngularProj/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 
 @Component({
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  constructor(){}
+  ngOnInit(){}
 }
--- AngularProj/src/app/app.module.ts
+++ AngularProj/src/app/app.module.ts
@@ -4,16 +4,19 @@ import {FormsModule}from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { ProjectlistComponent } from './projectlist/projectlist.component';
-import { ProductComponent } from './product/product.component';
+import { HeroesComponent } from './heroes/heroes.component';import { ParentCompComponent } from '../parent-comp/parent-comp.component';
+import  {ChildCompComponent} from  '../parent-comp/child-comp/child-comp.component';
+import { StudentComponent } from './student/student.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    ProjectlistComponent,
-    ProductComponent
+    ParentCompComponent,
+    ChildCompComponent,
+    StudentComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
