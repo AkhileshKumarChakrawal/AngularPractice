2eb9fb2af40b5396f321a62fe40348c439528527
akhilesh <akhilesh.chakrawal@india.semanticbits.com>
2020-02-10 19:33:51 +0530

    worker on router

--- AngularProj/src/app/app.module.ts
+++ AngularProj/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule}from '@angular/forms'
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';import { ParentCompComponent } from '../parent-comp/parent-comp.component';
-import  {ChildCompComponent} from  '../parent-comp/child-comp/child-comp.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { ParentCompComponent } from '../parent-comp/parent-comp.component';
+import {ChildCompComponent} from '../parent-comp/child-comp/child-comp.component';
 import { StudentComponent } from './student/student.component';
 import { StudentListComponent } from './student-list/student-list.component';
 
